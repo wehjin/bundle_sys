50c47761ba84d9b9bdff8f73b69f81016268fe0d
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-07-13 18:35:03 -0700

    Converts bundle methods into functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,72 @@
-use std::any::type_name_of_val;
+pub mod bundle {
+	use std::any::type_name_of_val;
 
-use web_sys::js_sys::{Object, Reflect};
-use web_sys::wasm_bindgen::{JsCast, JsValue};
+	use web_sys::js_sys::{Object, Reflect};
+	use web_sys::wasm_bindgen::{JsCast, JsValue};
 
-pub trait BunKey: Copy {
-	fn as_str(&self) -> &'static str { type_name_of_val(self) }
-	fn to_js(&self) -> JsValue { self.as_str().into() }
-	fn get<V: JsCast>(self, bun: &DataBun) -> Option<V> { bun.get(self) }
-}
-
-#[derive(Copy, Clone)]
-pub struct HelloKey;
-impl BunKey for HelloKey {}
-
-#[derive(Debug)]
-pub struct DataBun(Object);
-impl DataBun {
-	pub fn new() -> Self {
-		Self(Object::new())
+	pub trait Key: Copy {
+		fn as_str(&self) -> &'static str { type_name_of_val(self) }
+		fn to_js(&self) -> JsValue { self.as_str().into() }
+		fn get<V: JsCast>(self, bundle: &Bundle) -> Option<V> { get(&bundle, self) }
 	}
-	pub fn get<V: JsCast>(&self, key: impl BunKey) -> Option<V>
-	{
-		match Reflect::get(&self.0, &key.to_js()) {
+
+
+	pub type Bundle = Object;
+
+	pub fn new() -> Bundle { Object::new() }
+
+	pub fn get<V: JsCast>(b: &Bundle, key: impl Key) -> Option<V> {
+		match Reflect::get(b, &key.to_js()) {
 			Ok(js_value) if js_value.is_undefined() => None,
 			Ok(js_value) => Some(V::unchecked_from_js(js_value)),
 			Err(_) => None
 		}
 	}
-	pub fn assoc(&self, key: impl BunKey, value: impl JsCast) -> Self {
+	pub fn assoc(b: &Bundle, key: impl Key, value: impl JsCast) -> Bundle {
 		let object = Object::new();
-		Object::assign(&object, &self.0);
+		Object::assign(&object, &b);
 		Reflect::set(&object, &key.to_js(), &value.unchecked_into()).expect("reflect-set");
-		Self(object)
+		object
 	}
-	pub fn dissoc(&self, key: impl BunKey) -> Self {
+	pub fn dissoc(b: &Bundle, key: impl Key) -> Bundle {
 		let object = Object::new();
-		Object::assign(&object, &self.0);
+		Object::assign(&object, &b);
 		Reflect::delete_property(&object, &key.to_js()).expect("reflect-delete");
-		Self(object)
-	}
-}
-
-#[cfg(test)]
-pub mod tests {
-	use wasm_bindgen_test::wasm_bindgen_test;
-	use web_sys::js_sys::JsString;
-
-	use super::*;
-
-	#[wasm_bindgen_test]
-	fn no_value_when_empty() {
-		let bun = DataBun::new();
-		assert_eq!(None, HelloKey.get::<JsString>(&bun));
+		object
 	}
 
-	#[wasm_bindgen_test]
-	fn some_value_after_assoc() {
-		let bun = DataBun::new()
-			.assoc(HelloKey, JsString::from("Bob"))
-			;
-		assert_eq!(Some(JsString::from("Bob")), HelloKey.get(&bun));
-	}
+	#[cfg(test)]
+	pub mod tests {
+		use wasm_bindgen_test::wasm_bindgen_test;
+		use web_sys::js_sys::JsString;
 
-	#[wasm_bindgen_test]
-	fn no_value_after_dissoc() {
-		let bun = DataBun::new()
-			.assoc(HelloKey, JsString::from("Bob"))
-			.dissoc(HelloKey)
-			;
-		assert_eq!(None, HelloKey.get::<JsString>(&bun));
+		use crate::bundle;
+		use crate::bundle::{assoc, dissoc, Key};
+
+		#[derive(Copy, Clone)]
+		pub struct HelloKey;
+		impl Key for HelloKey {}
+
+
+		#[wasm_bindgen_test]
+		fn no_value_when_empty() {
+			let bun = bundle::new();
+			assert_eq!(None, HelloKey.get::<JsString>(&bun));
+		}
+
+		#[wasm_bindgen_test]
+		fn some_value_after_assoc() {
+			let bun = bundle::new();
+			let bun = assoc(&bun, HelloKey, JsString::from("Bob"));
+			assert_eq!(Some(JsString::from("Bob")), HelloKey.get(&bun));
+		}
+
+		#[wasm_bindgen_test]
+		fn no_value_after_dissoc() {
+			let bun = bundle::new();
+			let bun = assoc(&bun, HelloKey, JsString::from("Bob"));
+			let bun = dissoc(&bun, HelloKey);
+			assert_eq!(None, HelloKey.get::<JsString>(&bun));
+		}
 	}
 }
