7825bcf34727cf8598e8a50f3b5b0ef30194e876
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-07-13 21:21:51 -0700

    Removes ShadowKey hack.

--- src/lib.rs
+++ src/lib.rs
@@ -10,25 +10,18 @@ pub mod bundle {
 	impl<T: Key> Key for Box<T> {}
 
 	pub trait Get {
-		fn get<V: JsCast>(self, b: &Bundle) -> Option<V>;
+		fn get<V: JsCast>(&self, b: &Bundle) -> Option<V>;
 	}
 
 	impl<A: Key> Get for A {
-		fn get<V: JsCast>(self, b: &Bundle) -> Option<V> { get(&b, self) }
+		fn get<V: JsCast>(&self, b: &Bundle) -> Option<V> { get(&b, self) }
 	}
-
-	#[derive(Clone)]
-	struct ShadowKey(JsValue);
-	impl Key for ShadowKey {
-		fn to_js(&self) -> JsValue { self.0.clone() }
-	}
-
 
 	pub type Bundle = Object;
 
 	pub fn empty() -> Bundle { Object::new() }
 
-	pub fn get<V: JsCast>(b: &Bundle, key: impl Key) -> Option<V> {
+	pub fn get<V: JsCast>(b: &Bundle, key: &dyn Key) -> Option<V> {
 		match Reflect::get(b, &key.to_js()) {
 			Ok(js_value) if js_value.is_undefined() => None,
 			Ok(js_value) => Some(V::unchecked_from_js(js_value)),
@@ -38,22 +31,22 @@ pub mod bundle {
 	pub fn get_in<V: JsCast>(b: &Bundle, keys: &[&dyn Key]) -> Option<V> {
 		match keys.len() {
 			0 => None,
-			1 => get::<V>(b, ShadowKey(keys[0].to_js())),
+			1 => get::<V>(b, keys[0]),
 			_ => {
 				let mut b = b.clone();
 				for i in 0..(keys.len() - 1) {
-					match get::<Bundle>(&b, ShadowKey(keys[i].to_js())) {
+					match get::<Bundle>(&b, keys[i]) {
 						None => return None,
 						Some(child) => {
 							b = child;
 						}
 					}
 				}
-				get::<V>(&b, ShadowKey(keys[keys.len() - 1].to_js()))
+				get::<V>(&b, keys[keys.len() - 1])
 			}
 		}
 	}
-	pub fn assoc(b: &Bundle, key: impl Key, value: impl JsCast) -> Bundle {
+	pub fn assoc(b: &Bundle, key: &dyn Key, value: impl JsCast) -> Bundle {
 		let object = Object::new();
 		Object::assign(&object, &b);
 		Reflect::set(&object, &key.to_js(), &value.unchecked_into()).expect("reflect-set");
@@ -62,13 +55,13 @@ pub mod bundle {
 	pub fn assoc_in(b: &Bundle, keys: &[&dyn Key], value: impl JsCast) -> Bundle {
 		match keys.len() {
 			0 => copy(b),
-			1 => assoc(b, ShadowKey(keys[0].to_js()), value),
+			1 => assoc(b, keys[0], value),
 			_ => {
-				let value = match get::<Bundle>(b, ShadowKey(keys[0].to_js())) {
+				let value = match get::<Bundle>(b, keys[0]) {
 					None => assoc_in(&empty(), &keys[1..], value),
 					Some(existing) => assoc_in(&existing, &keys[1..], value),
 				};
-				assoc(b, ShadowKey(keys[0].to_js()), value)
+				assoc(b, keys[0], value)
 			}
 		}
 	}
@@ -108,29 +101,29 @@ pub mod bundle {
 
 		#[wasm_bindgen_test]
 		fn some_value_after_assoc() {
-			let bun = assoc(&empty(), WorldKey, JsString::from("Bob"));
+			let bun = assoc(&empty(), &WorldKey, JsString::from("Bob"));
 			assert_eq!(Some(JsString::from("Bob")), WorldKey.get(&bun));
 		}
 
 		#[wasm_bindgen_test]
 		fn no_value_after_dissoc() {
-			let bun = assoc(&empty(), WorldKey, JsString::from("Bob"));
+			let bun = assoc(&empty(), &WorldKey, JsString::from("Bob"));
 			let bun = dissoc(&bun, WorldKey);
 			assert_eq!(None, WorldKey.get::<JsString>(&bun));
 		}
 
 		#[wasm_bindgen_test]
 		fn assoc_after_assoc_leaves_parent_untouched() {
-			let a = assoc(&empty(), WorldKey, JsString::from("Bob"));
-			let b = assoc(&a, WorldKey, JsString::from("Silent"));
+			let a = assoc(&empty(), &WorldKey, JsString::from("Bob"));
+			let b = assoc(&a, &WorldKey, JsString::from("Silent"));
 			assert_ne!(WorldKey.get::<JsString>(&a), WorldKey.get::<JsString>(&b));
 		}
 
 		#[wasm_bindgen_test]
 		fn assoc_in_creates_sub_bundles() {
 			let parent = assoc_in(&empty(), &[&HelloKey, &WorldKey], JsString::from("Bob"));
-			let child = bundle::get::<Bundle>(&parent, HelloKey).unwrap();
-			let from_child = bundle::get::<JsString>(&child, WorldKey);
+			let child = bundle::get::<Bundle>(&parent, &HelloKey).unwrap();
+			let from_child = bundle::get::<JsString>(&child, &WorldKey);
 			let from_parent = bundle::get_in::<JsString>(&parent, &[&HelloKey, &WorldKey]);
 			assert_eq!(from_child, from_parent);
 		}
