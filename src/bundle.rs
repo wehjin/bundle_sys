1cca99ccf8728664ef00306a35feaabe5b25bb5e
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-07-14 14:38:57 -0700

    Moves back to string keys.

--- src/bundle.rs
+++ src/bundle.rs
@@ -1,41 +1,41 @@
-use std::any::{type_name, type_name_of_val};
+use std::any::Any;
+use std::collections::HashMap;
+use std::fmt::Debug;
 use std::rc::Rc;
 
-use anymap::CloneAny;
+#[derive(Debug, Clone)]
+struct BundleValue(Rc<dyn Any>);
 
-use crate::key::Key;
+impl BundleValue {
+	pub fn new<V: Clone + 'static>(value: V) -> Self {
+		Self(Rc::new(value))
+	}
+	pub fn get_rc<V: Clone + 'static>(&self) -> Rc<V> {
+		self.0.clone().downcast::<V>().expect("downcast")
+	}
+}
 
-type CloneMap = anymap::hashbrown::Map<dyn CloneAny>;
-
-#[derive(Clone)]
-struct Property<K, V>(pub K, pub V)
-where
-	K: Key,
-	V: Clone + 'static;
-
-pub struct Bundle(CloneMap);
+#[derive(Debug)]
+pub struct Bundle(HashMap<String, BundleValue>);
 impl Bundle {
-	pub fn empty() -> Self { Self(CloneMap::new()) }
+	pub fn empty() -> Self { Self(HashMap::new()) }
 	pub fn len(&self) -> usize { self.0.len() }
 
-	pub fn get<K: Key, V: Clone + 'static>(&self) -> Option<&Rc<V>>
+	pub fn get<V: Clone + 'static>(&self, key: impl AsRef<str>) -> Option<Rc<V>>
 	{
-		if let Some(Property(key, value)) = self.0.get::<Property<K, Rc<V>>>() {
-			debug_assert_eq!(type_name_of_val(key), type_name::<K>());
-			return Some(value);
-		}
-		None
+		self.0.get(key.as_ref())
+			.map(|value| value.get_rc::<V>())
 	}
-	pub fn assoc<K: Key, V: Clone + 'static>(&self, key: K, value: V) -> Self
+	pub fn assoc<V: Clone + 'static>(&self, key: impl AsRef<str>, value: V) -> Self
 	{
 		let mut data = self.0.clone();
-		data.insert(Property(key, Rc::new(value)));
+		data.insert(key.as_ref().to_string(), BundleValue::new(value));
 		Self(data)
 	}
-	pub fn dissoc<K: Key, V: Clone + 'static>(&self) -> Self
+	pub fn dissoc<V: Clone + 'static>(&self, key: impl AsRef<str>) -> Self
 	{
 		let mut data = self.0.clone();
-		data.remove::<Property<K, Rc<V>>>();
+		data.remove(key.as_ref());
 		Self(data)
 	}
 }
@@ -43,60 +43,66 @@ impl Bundle {
 #[cfg(test)]
 pub mod tests {
 	use crate::bundle::Bundle;
-	use crate::fixtures::{Hello, World};
 
 	#[test]
 	fn no_value_when_empty() {
 		let bundle = Bundle::empty();
 		assert_eq!(bundle.len(), 0);
-		assert_eq!(bundle.get::<Hello, String>(), None)
+		assert_eq!(bundle.get::<String>("hello"), None)
 	}
 
 	#[test]
-	fn some_value_after_assoc_no_value_after_dissoc() {
+	fn stores_different_types() {
+		let bundle = Bundle::empty()
+			.assoc("hello", "Bob".to_string())
+			.assoc("world", 32usize)
+			;
+		assert_eq!(bundle.len(), 2);
+		assert_eq!(bundle.get::<String>("hello").unwrap().as_str(), "Bob");
+		assert_eq!(bundle.get::<usize>("world").unwrap().as_ref(), &32usize);
+	}
+
+	#[test]
+	fn some_value_after_assoc_and_no_value_after_dissoc() {
 		let first = Bundle::empty()
-			.assoc(Hello, "Bob".to_string())
+			.assoc("hello", "Bob".to_string())
 			;
 		assert_eq!(first.len(), 1);
-		assert_eq!(first.get::<Hello, String>().unwrap().as_str(), "Bob");
+		assert_eq!(first.get::<String>("hello").unwrap().as_str(), "Bob");
 
 		let second = first
-			.dissoc::<Hello,String>()
+			.dissoc::<String>("hello")
 			;
 		assert_eq!(first.len(), 1);
-		assert_eq!(first.get::<Hello, String>().unwrap().as_str(), "Bob");
+		assert_eq!(first.get::<String>("hello").unwrap().as_str(), "Bob");
 		assert_eq!(second.len(), 0);
-		assert_eq!(second.get::<Hello, String>(), None);
+		assert_eq!(second.get::<String>("hello"), None);
 	}
 
 	#[test]
 	fn same_key_with_second_value_replaces_value_only_in_second_bundle() {
-		let first = Bundle::empty()
-			.assoc(Hello, "Bob".to_string())
-			;
-		let second = first
-			.assoc(Hello, "Marley".to_string())
-			;
+		let first = Bundle::empty().assoc("hello", "Bob".to_string());
+		let second = first.assoc("hello", "Marley".to_string());
 		assert_eq!(first.len(), 1);
-		assert_eq!(first.get::<Hello, String>().unwrap().as_ref(), "Bob");
+		assert_eq!(first.get::<String>("hello").unwrap().as_ref(), "Bob");
 		assert_eq!(second.len(), 1);
-		assert_eq!(second.get::<Hello, String>().unwrap().as_ref(), "Marley");
+		assert_eq!(second.get::<String>("hello").unwrap().as_ref(), "Marley");
 	}
 
 	#[test]
 	fn second_key_adds_second_value_only_in_second_bundle() {
 		let first = Bundle::empty()
-			.assoc(Hello, "Jack".to_string())
+			.assoc("hello", "Jack".to_string())
 			;
 		let second = first
-			.assoc(World, "Jill".to_string())
+			.assoc("world", "Jill".to_string())
 			;
 		assert_eq!(first.len(), 1);
-		assert_eq!(first.get::<Hello, String>().unwrap().as_ref(), "Jack");
-		assert_eq!(first.get::<World, String>(), None);
+		assert_eq!(first.get::<String>("hello").unwrap().as_ref(), "Jack");
+		assert_eq!(first.get::<String>("world"), None);
 		assert_eq!(second.len(), 2);
-		assert_eq!(second.get::<Hello, String>().unwrap().as_ref(), "Jack");
-		assert_eq!(second.get::<World, String>().unwrap().as_ref(), "Jill");
+		assert_eq!(second.get::<String>("hello").unwrap().as_ref(), "Jack");
+		assert_eq!(second.get::<String>("world").unwrap().as_ref(), "Jill");
 	}
 
 
@@ -105,14 +111,14 @@ pub mod tests {
 	fn assoc_in_creates_sub_bundles() {
 		// let hello_and_world = assoc_in(
 		// 	&empty(),
-		// 	[Hello.as_key(), World.as_key()],
+		// 	[HELLO.as_key(), WORLD.as_key()],
 		// 	JsString::from("Bob"),
 		// );
-		// let just_world = bundle::get::<Bundle>(&hello_and_world, &Hello).unwrap();
-		// let bob_from_just_world = bundle::get::<JsString>(&just_world, &World);
+		// let just_world = bundle::get::<Bundle>(&hello_and_world, &HELLO).unwrap();
+		// let bob_from_just_world = bundle::get::<JsString>(&just_world, &WORLD);
 		// let bob_from_hello_world = bundle::get_in::<JsString>(
 		// 	&hello_and_world,
-		// 	[Hello.as_key(), World.as_key()],
+		// 	[HELLO.as_key(), WORLD.as_key()],
 		// );
 		// assert_eq!(bob_from_just_world, bob_from_hello_world);
 	}
--- src/fixtures.rs
+++ src/fixtures.rs
@@ -1,9 +1,1 @@
-use crate::key::Key;
 
-#[derive(Copy, Clone, PartialEq)]
-pub struct Hello;
-impl Key for Hello {}
-
-#[derive(Copy, Clone, PartialEq)]
-pub struct World;
-impl Key for World {}
