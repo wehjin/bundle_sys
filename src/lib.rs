22f30f717ae213a955bdec255228559f99d49843
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-07-13 22:36:26 -0700

    Renames TryGet back to Get.

--- src/lib.rs
+++ src/lib.rs
@@ -14,19 +14,19 @@ pub mod bundle {
 	}
 	impl<T: Key> Key for Box<T> {}
 
-	pub trait TryGet {
-		fn try_get<V: JsCast>(&self, b: &Bundle) -> Option<V>;
+	pub trait Get {
+		fn get<V: JsCast>(&self, b: &Bundle) -> Option<V>;
 	}
 
-	impl<A: Key> TryGet for A {
-		fn try_get<V: JsCast>(&self, b: &Bundle) -> Option<V> { try_get(&b, self) }
+	impl<A: Key> Get for A {
+		fn get<V: JsCast>(&self, b: &Bundle) -> Option<V> { get(&b, self) }
 	}
 
 	pub type Bundle = Object;
 
 	pub fn empty() -> Bundle { Object::new() }
 
-	pub fn try_get<V: JsCast>(b: &Bundle, key: &dyn Key) -> Option<V>
+	pub fn get<V: JsCast>(b: &Bundle, key: &dyn Key) -> Option<V>
 	{
 		match Reflect::get(b, &key.to_js()) {
 			Ok(js_value) if js_value.is_undefined() => None,
@@ -34,23 +34,23 @@ pub mod bundle {
 			Err(_) => None
 		}
 	}
-	pub fn try_get_in<V: JsCast>(b: &Bundle, keys: impl AsRef<[&'static dyn Key]>) -> Option<V>
+	pub fn get_in<V: JsCast>(b: &Bundle, keys: impl AsRef<[&'static dyn Key]>) -> Option<V>
 	{
 		let keys = keys.as_ref();
 		match keys.len() {
 			0 => None,
-			1 => try_get::<V>(b, keys[0]),
+			1 => get::<V>(b, keys[0]),
 			_ => {
 				let mut b = b.clone();
 				for i in 0..(keys.len() - 1) {
-					match try_get::<Bundle>(&b, keys[i]) {
+					match get::<Bundle>(&b, keys[i]) {
 						None => return None,
 						Some(child) => {
 							b = child;
 						}
 					}
 				}
-				try_get::<V>(&b, keys[keys.len() - 1])
+				get::<V>(&b, keys[keys.len() - 1])
 			}
 		}
 	}
@@ -68,7 +68,7 @@ pub mod bundle {
 			0 => copy(b),
 			1 => assoc(b, keys[0], value),
 			_ => {
-				let value = match try_get::<Bundle>(b, keys[0]) {
+				let value = match get::<Bundle>(b, keys[0]) {
 					None => assoc_in(&empty(), &keys[1..], value),
 					Some(existing) => assoc_in(&existing, &keys[1..], value),
 				};
@@ -96,7 +96,7 @@ pub mod bundle {
 		use web_sys::js_sys::JsString;
 
 		use crate::bundle;
-		use crate::bundle::{assoc, assoc_in, Bundle, dissoc, empty, Key, TryGet};
+		use crate::bundle::{assoc, assoc_in, Bundle, dissoc, empty, Get, Key};
 
 		#[derive(Copy, Clone)]
 		pub struct Hello;
@@ -109,27 +109,27 @@ pub mod bundle {
 		#[wasm_bindgen_test]
 		fn no_value_when_empty() {
 			let bun = empty();
-			assert_eq!(None, World {}.try_get::<JsString>(&bun));
+			assert_eq!(None, World.get::<JsString>(&bun));
 		}
 
 		#[wasm_bindgen_test]
 		fn some_value_after_assoc() {
 			let bun = assoc(&empty(), &World, JsString::from("Bob"));
-			assert_eq!(Some(JsString::from("Bob")), World.try_get(&bun));
+			assert_eq!(Some(JsString::from("Bob")), World.get(&bun));
 		}
 
 		#[wasm_bindgen_test]
 		fn no_value_after_dissoc() {
 			let bun = assoc(&empty(), &World, JsString::from("Bob"));
 			let bun = dissoc(&bun, World);
-			assert_eq!(None, World.try_get::<JsString>(&bun));
+			assert_eq!(None, World.get::<JsString>(&bun));
 		}
 
 		#[wasm_bindgen_test]
 		fn assoc_after_assoc_leaves_parent_untouched() {
 			let a = assoc(&empty(), &World, JsString::from("Bob"));
 			let b = assoc(&a, &World, JsString::from("Silent"));
-			assert_ne!(World.try_get::<JsString>(&a), World.try_get::<JsString>(&b));
+			assert_ne!(World.get::<JsString>(&a), World.get::<JsString>(&b));
 		}
 		#[wasm_bindgen_test]
 		fn assoc_in_creates_sub_bundles() {
@@ -138,9 +138,9 @@ pub mod bundle {
 				[Hello.as_key(), World.as_key()],
 				JsString::from("Bob"),
 			);
-			let just_world = bundle::try_get::<Bundle>(&hello_and_world, &Hello).unwrap();
-			let bob_from_just_world = bundle::try_get::<JsString>(&just_world, &World);
-			let bob_from_hello_world = bundle::try_get_in::<JsString>(
+			let just_world = bundle::get::<Bundle>(&hello_and_world, &Hello).unwrap();
+			let bob_from_just_world = bundle::get::<JsString>(&just_world, &World);
+			let bob_from_hello_world = bundle::get_in::<JsString>(
 				&hello_and_world,
 				[Hello.as_key(), World.as_key()],
 			);
